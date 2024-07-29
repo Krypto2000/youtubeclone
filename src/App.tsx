c75fed954f3b9202bbe0ceb8d363880a6d178e34
Krypto2000 <162322845+Krypto2000@users.noreply.github.com>
2024-07-29 12:55:53 +0100

    Completed Youtubeclone Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
 
+
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Watch from './pages/Watch'
+import Search from './pages/Search'
 export default function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>Hi</h1>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/search' element={<Search/>}/>
+    <Route path='/watch/:id' element={<Watch/>}/>
+    <Route path='/' element={<Home/>}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
+
